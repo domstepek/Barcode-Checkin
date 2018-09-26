443f9cf75d73af47ff4b0b1baf85c7e61eb3c831
Dominique Stepek <domstepek@gmail.com>
2018-09-26 16:09:08 -0700

    Working ARDUINO code, correct spreadsheet ID

--- checkin.py
+++ checkin.py
@@ -17,8 +17,8 @@ class SerialWrapper:
         self.ser.write(data.encode())
 
 def main():
-    #ser = SerialWrapper('/dev/ttyACM0') # Connect to arduino
-    SPREADSHEET_IDS = '1OZGMLB5fagiBeAG8-FHYKJ7YbWZvw7fJonjZjkVyDYI' # Spreadsheet ID for list of ids, names, and clubs
+    ser = SerialWrapper('/dev/ttyACM0') # Connect to arduino
+    SPREADSHEET_IDS = '1xvvscFla3_cC6NXWc1wduA6EzomtyYkLeP8LMxQkGtc' # Spreadsheet ID for list of ids, names, and clubs
     SPREADSHEET_LOG = '1bpBm-T6QB6inECZl_hKLomBEZMoUAe5GaMM-yyHtoG4' # Spreadsheet ID for logging ids, names, and clubs
     RANGE_IDS = 'Form Responses 1!A2:D'
     RANGE_LOG = 'Sheet1!A2:D'
@@ -38,7 +38,7 @@ def main():
                 for x in values:
                     if (any(id in j for j in x)): # Checks if any ID in IDS variable match scanned ID
                         s = "Welcome %s to %s!" % (x[1], x[3]) # Output to arduino
-                        #ser.sendData(s)
+                        ser.sendData(s)
 
                         now = datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S') # Gets current time and formats
                         resource = {
@@ -52,6 +52,7 @@ def main():
                             body=resource,
                             valueInputOption="RAW"
                         ).execute() # Append new line to LOG sheet
+                        break
 
 if __name__ == '__main__':
     main()
